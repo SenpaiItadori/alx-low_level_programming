916418af6490439bb5965942dfcf9d62a602d6ce
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-14 13:43:23 -0800

    like naturec made the natural nums

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main -prints sum of all the multiples of three and five below 1024
+ * Return: i have no idea but if i were to guess i would say the result
+ */
+
+int main(void)
+{
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	z += i;
+	}
+	i++;
+	}
+	printsf("%d\n", z);
+	return (0);
+}
