507fccad08c009a06f6354eeda7b68f01a67a17c
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-14 13:28:15 -0800

    the world looks like a multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,32 +8,32 @@
 
 void print_times_table(int n)
 {
-	int num, mpl, res;
+	int num, mple, rest;
 
 	if (n >= 0 && n <= 15)
 	{
 	for (num = 0; num <= n; num++)
 	{
 	_putchar('0');
-	for (mpl = 1; mpl <= n; mpl++)
+	for (mple = 1; mple <= n; mple++)
 	{
 	_putchar(',');
 	_putchar(' ');
-	res = num * mpl;
-	if (res == 99)
+	rest = num * mple;
+	if (rest == 99)
 	_putchar(' ');
-	if (res <= 9)
+	if (rest <= 9)
 	_putchar(' ');
-	if (res >= 100)
+	if (rest >= 100)
 	{
-	_putchar((res / 100) + '0');
-	_putchar((res / 10) % 10 + '0');
+	_putchar((rest / 100) + '0');
+	_putchar((rest / 10) % 10 + '0');
 	}
-	else if (res <= 99 && res >= 10)
+	else if (rest <= 99 && rest >= 10)
 	{
-	_putchar((res / 10) + '0');
+	_putchar((rest / 10) + '0');
 	}
-	_putchar((res % 10) + '0');
+	_putchar((rest % 10) + '0');
 	}
 	_putchar('\n');
 	}
