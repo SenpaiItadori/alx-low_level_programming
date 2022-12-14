1db1bdba05a7c9ac6b63f087bda719e77b33b5e3
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-14 08:39:12 -0800

    last digit again

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_last_digit - displays the last digit of y
+ * @n: the initial num
+ * @y: the last num
+ * Return: y
+ */
+
+int print_last_digit(int n)
+{
+	int y;
+
+	y = n % 10;
+
+	return (y);
+}
