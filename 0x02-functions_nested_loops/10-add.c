189cfa6ac4189c6651a406f151d6e91bc85ca746
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-14 10:21:12 -0800

    n plus b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * add - literally adds bruh
+ * @n: first num to be  computed
+ * @b: the other num
+ * Return: the sum of the integers
+ */
+
+int add(int n, int b)
+{
+	return (n+b);
+}
