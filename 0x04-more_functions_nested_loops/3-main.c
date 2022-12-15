231345b6041a4768e50d6fbcbf4264f98e55a7bb
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-15 13:04:36 -0800

    print 1-10

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * main - code check
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_numbers - deletes numbers lol
+ * Return: the numbers
+ */
+
+void print_numbers(void)
+{
+	int numb;
+	for (numb = 48; numb < 58; numb++)
+	{
+		_putchar(numb);
+	}
+	_putchar('\n');
+}
