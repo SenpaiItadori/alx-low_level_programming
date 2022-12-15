c0d4d93ecdd66b2856657e9b0aaf31217513775a
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-15 12:21:33 -0800

    collabs are now interestingly multiplications by alx

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * mul - multiples 2 integers
+ * @a: is one of em
+ * @b:is another one of em integers we talked abt
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
