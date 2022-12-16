eca47939c5df3b519a8a918c1be2d5bb00e6e25a
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-16 06:07:27 -0800

    i just printed up diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: number of times \ is printed
+ * Return: aint no return yall
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (i = 0; i < j; i++)
+				putchar(32);
+			putchar(92);
+			putchar(10);
+		}
+		putchar(10);
+	}
+	else
+		putchar(10);
+}
