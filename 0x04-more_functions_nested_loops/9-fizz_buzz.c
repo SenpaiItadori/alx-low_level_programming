4bc2343edb9681e6d8a61ffcbd3881840b7afc8b
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-16 07:17:39 -0800

    the fizz nbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100 followd by a new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
