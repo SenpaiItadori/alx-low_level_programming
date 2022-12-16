1e5dc81fe7d80b6a6208d1b8cd7f75c366dc5a8e
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-16 11:26:02 -0800

    not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-print_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_number - doesnt tdo that at all does it?
+ * @n: to be printed
+ * return: n/a
+ */
+
+void print_number(int n)
+{
+
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		i = n;
+		putchar('-');
+	}
+	i /= 10;
+	if (i != 0)
+		print_number(i);
+	putchar((unsigned int) n % 10 + '0');
+}
