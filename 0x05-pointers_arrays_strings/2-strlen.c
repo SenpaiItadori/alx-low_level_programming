33ea811dcfc9d6bbaa960c41f5925ef7d3f7a107
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-19 08:53:43 -0800

    this report by ts very length defents itsself from audiance

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - returns length of string
+ * @s: the string to be measured
+ * Return: 0
+ */
+
+int _strlen(char *str)
+{
+	return (strlen(str));
+}
