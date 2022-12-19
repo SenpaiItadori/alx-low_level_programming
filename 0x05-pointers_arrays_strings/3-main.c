3e6b8da00ce88afd674dece3f3b26aa3ff6c57c5
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-19 09:14:17 -0800

    i dont fear pcs i fear the  lack of em

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main - code checker
+ * Return: 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "F you very very much";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: the string
+ * Return: n/a
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
