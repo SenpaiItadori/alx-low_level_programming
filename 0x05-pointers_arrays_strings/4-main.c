8a5e21ee9cffd7997da046e94dc4d59d0720edd7
SenpaiItadori <takudzwarnyoni00@gmail.com>
2022-12-19 09:52:28 -0800

    what the hell is  this

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * main - code checker
+ * Return: 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "nayR yeH";
+	print_rev(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * print_rev - prints character in reverse
+ * @str: the string or character to be reversed
+ * Return: n/a
+ */
+
+void print_rev(char *str)
+{
+
+	puts(strrev(str));
+}
