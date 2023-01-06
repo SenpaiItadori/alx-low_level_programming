797be6394b2a795d2efb8b517cdebd3295324df1
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-06 10:45:44 -0800

    the best arg against democracy is a 5 min alk with the av voter

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints all the arguments it recieves
+ * argc: the count of arguments that run the file
+ * argv: an array containing the program comand line arguments
+ * Rerurn: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
