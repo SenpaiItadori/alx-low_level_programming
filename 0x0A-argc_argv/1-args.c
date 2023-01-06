33167bf80c076bf39e6b5f519629cc744a55d417
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-06 08:20:42 -0800

    silence is carried out by other menas

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints program name vi argv and argc
+ * @argv: an array containing the program command line arguments
+ * @argc: the size of the argv array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
