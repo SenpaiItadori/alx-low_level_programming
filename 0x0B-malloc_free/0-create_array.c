d6c0a5730735c7ed0af7f880f6441a8e9a72255c
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-10 06:59:07 -0800

    float like butterfly, but oh boy

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,14 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
 	char *yes = (char *) malloc(sizeof(char) * size);
 
-	yes[0] = c;
-	if (size == 0)
+	if (size == 0 || yes == NULL)
 	{
 		return (NULL);
 	}
+	for (i = 0; i < size; i++)
+		yes[i] = c;
 	return (yes);
 }
