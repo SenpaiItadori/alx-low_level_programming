8e98a5341d011f1a73faf7da1d7e0811e5e30cbb
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-10 06:39:56 -0800

    float like butterfly, but oh boy

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars and initializes it a specific char
+ * @size: the size of the array
+ * @c: the specific char
+ * Return: Null is size = 0, pointer to the array or Null if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *yes = (char*) malloc(sizeof(char) * size);
+
+	yes[0] = c;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	return (yes);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated spacein mem
+ * @str: the pointer
+ * Return: *str
+ */
+
+char *_strdup(char *str)
+{
+
+}
