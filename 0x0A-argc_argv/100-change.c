9fcf6512ae8cb9ae49fabc528c0257763b0a985a
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-11 08:02:25 -0800

    min num of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints the min amount of coins to make change for an amount of money
- * @argv: programm run arguments string of arrays whre the amount would be input
+ * @argv: program run arguments string of arrays whre amount would be input
  * @argc: string length of argv
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	unsigned int i, j, k, n, x, y, z;
+	unsigned int i, j, k, n, x, y = 1;
 
 	if (argc != 2)
 	{
@@ -22,14 +21,11 @@ int main(int argc, char *argv[])
 	k = (i % 25) / 10;
 	n = (i % 10) / 5;
 	x = (i % 5) / 2;
-	z = 1;
-	y = 0;
-
 	if (i <= 0)
 		printf("0\n");
 	if (argc == 2)
 	{
-		if(i % 25 != 0)
+		if (i % 25 != 0)
 		{
 			if (i % 10 != 0)
 			{
@@ -37,7 +33,7 @@ int main(int argc, char *argv[])
 				{
 					if (i % 2 != 0)
 					{
-						y = y + k + j + n + x + z;
+						y = y + k + j + n + x;
 						printf("%d\n", y);
 					}
 					else
