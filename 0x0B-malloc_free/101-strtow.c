f191f268f3370d3c3b9973626ae775f52f5176f4
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-11 12:21:14 -0800

    somn abt climbing mountains

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow - spits a string into 2 words
+ * @str: the string to be split
+ * Return: the two words if success or nUll if fail or strlen = 0
+ */
+
+char **strtow(char *str)
+{
+
+}
