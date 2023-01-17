b2cb79ad132a40506d61b842c1e25fd3dd7daef9
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-17 07:49:53 -0800

    oh boy Rick

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,21 +9,22 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-	{
-		return;
-	}
 
-	else
+	if (d != NULL)
 	{
-		if (((*d).name) == NULL)
+		if ((d->name) == NULL)
 			printf("Name: (nil)\n");
-		printf("Name: %s\n", (*d).name);
-		if (!((*d).age))
+		else
+			printf("Name: %s\n", (*d).name);
+		if (d->age < 0)
 			printf("Age: (nil)");
-		printf("Age %f\n", (*d).age);
-		if ((*d).owner == NULL)
+		else
+			printf("Age %f\n", (*d).age);
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
-		printf("Owner: %s\n", (*d).owner);
+		else
+			printf("Owner: %s\n", (*d).owner);
 	}
+	else
+		return;
 }
