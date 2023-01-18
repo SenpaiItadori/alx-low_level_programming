546a8fd5de54975a96eaea0a3717c798d991a8ce
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-18 11:30:21 -0800

    tiny correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[])
 	int(*func)(int, int);
 	char y;
 
-	func = get_op_func(argv[2]);
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -22,7 +21,7 @@ int main(int argc, char *argv[])
 	}
 	j = atoi(argv[1]);
 	k = atoi(argv[3]);
-
+	func = get_op_func(argv[2]);
 	if (!func)
 	{
 		printf("Error\n");
