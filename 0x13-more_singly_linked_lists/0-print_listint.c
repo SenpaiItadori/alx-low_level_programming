fe1da39d229e71d95eeef1f7bcb344be80d3a69d
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-30 10:35:41 -0800

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: a pointer to the contents of listint_t
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	i = sizeof(listint_t) / 8;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+Plane just landed
+I dont know what im saying but i know that linked lists arre fun as f
