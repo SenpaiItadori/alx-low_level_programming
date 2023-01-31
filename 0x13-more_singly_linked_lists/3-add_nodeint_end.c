415f7ad8784ad4a8ba6b2fce1cba6b67f9c77527
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-01-31 07:22:42 -0800

    free list 2

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,10 +15,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (*head == NULL)
+	if (*curr == NULL)
 	{
-		*head = new_node;
-		return (*head);
+		*curr = new_node;
+		return (new_node);
 	}
 
 	new_node->next = NULL;
