8c1f9fa644789eedd198071fd81a966f9920963c
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-04-06 10:46:01 -0700

    key->index

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
+	if (size <= 0)
+		return (NULL);
 	if (new_table == NULL)
 	{
 		return (NULL);
 	}
 	new_table->size = size;
+	new_table->array = malloc(size);
 
 	return (new_table);
 }
