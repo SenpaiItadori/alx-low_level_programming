fa48a0509c9f1ed2280180e80ed816c4a0953135
SenpaiItadori <takudzwarnyoni00@gmail.com>
2023-04-11 01:23:08 -0700

    key->index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,27 +9,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int i;
-	char **p;
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
-	hash_node_t *placement = malloc(sizeof(hash_node_t));
-
-
-	if (new_table == NULL)
-		return (0);
-
-	p = (char **)key;
-	new_table->size = size;
-	new_table->array = malloc(size);
-	placement->value = "";
-	placement->next = NULL;
-	for (i = 0; i < size; i++)
-	{
-		if (new_table->array[i] != NULL)
-		{
-			if (strcmp((placement->key), *p) == 0)
-				return (i);
-		}
-	}
-	return (0);
+	return (hash_djb2(key) % size);
 }
