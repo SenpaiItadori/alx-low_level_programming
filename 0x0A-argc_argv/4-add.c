eb5e35bf7b6e6eb51de91461dc49986ef8f03df3
SenpaiItadori <takudzwarnyoni@gmail.com>
2023-06-09 02:55:45 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - prints it's own program name and new line
+ * @argc: command line arguments count
+ * @argv: array of command line arguments
+ * Return: 0 on success ,else  1
+ */
+
+int main(int argc, char *argv[])
+{
+	int iteration = 1, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (iteration = 1; iteration < argc; iteration++)
+	{
+		if (!(atoi(argv[iteration])))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum = sum + atoi(argv[iteration]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
