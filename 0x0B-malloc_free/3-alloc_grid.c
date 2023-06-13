195ebaf5d14390b373bc7dc786155767d5a54550
SenpaiItadori <takudzwarnyoni@gmail.com>
2023-06-13 12:16:04 -0700

    If you even dream of beating me youd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,7 +29,7 @@ int **alloc_grid(int width, int height)
 
 		if (matrix[i] == NULL)
 		{
-			for (j = i; j == 0; j--)
+			for (j = 0; j < height; j++)
 			{
 				free(matrix[j]);
 				return (NULL);
