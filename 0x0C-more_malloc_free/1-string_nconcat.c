3a9476f493c27f5c4a39271ba43c920d242913a7
SenpaiItadori <takudzwarnyoni@gmail.com>
2023-06-15 04:43:12 -0700

    array_range

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,33 +10,43 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s3, *s4 = "\0";
-	int i = 0;
-	unsigned int size, j;
+	char *s3, *s4 = "";
+	unsigned int size, j, i = 0, k;
 
-	size = (strlen(s1) + 1) + n + 1;
+	if (s2 == NULL)
+		return (s1);
+	if (s1 == NULL)
+		return (s2);
+	if (s1 == NULL && s2 == NULL)
+		return (s4);
+
+	size = sizeof(s1) + n  + 1;
+	j = strlen(s2) + 1;
 
 	s3 = malloc(size);
 	if (s3 == NULL)
 		return (NULL);
-
-	if (s1 == NULL)
-		return (s2);
 
 	while (s1[i])
 	{
 		s3[i] = s1[i];
 		i++;
 	}
-	if (s2 == NULL)
-		return (s1);
-	if (s1 == NULL && s2 == NULL)
-		return (s4);
 
-	for (j = 0; j <= n; j++, i++)
+	if (n < strlen(s2))
 	{
-		s3[i] = s2[j];
+		for (k = i; k < (i + n); k++)
+		{
+			s3[k] = s2[k - i];
+		}
 	}
-	s3[i + 1] = s4[0];
+	else
+	{
+		for (k = i; k < (i + j); k++)
+		{
+			s3[k] = s2[k - i];
+		}
+	}
 	return (s3);
+
 }
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two positive numbers
+ * @argc - the number of command line arguments
+ * @argv - an array containing all command line arguments
+ * Return: 0 else 1
+ */
+
+int main(int argc, char **argv)
+{
+}
