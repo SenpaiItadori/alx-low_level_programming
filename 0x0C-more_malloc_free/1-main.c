34c6dbf5dda41e8da539d1516a34868bd5360bf5
SenpaiItadori <takudzwarnyoni@gmail.com>
2023-06-15 06:27:30 -0700

    array_range

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat(NULL, "School !!!", 3);
+    concat = string_nconcat("Ya ", "Bish !!", 2);
     printf("%s\n", concat);
     free(concat);
     return (0);
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,49 +10,60 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s3, *s4 = "\0";
+	char *s3, *s4 = "";
 	unsigned int size, j, i = 0, k;
 
 	size = sizeof(s1) + n  + 1;
 	j = strlen(s2) + 1;
 
-	s3 = malloc(size);
-	if (s3 == NULL)
-		return (NULL);
+	if (s1 == NULL && s2 == NULL)
+		return (s4);
 
-	if (s1 == NULL)
+	else if (s2 == NULL)
 	{
-		while (s2[i] && i < n)
+		s3 = malloc(sizeof(s1) + 1);
+		for (i = 0; s1[i]; i++)
+			s3[i] = s1[i];
+		return (s3);
+	}
+	else if (s1 == NULL)
+	{
+		if (n < sizeof(s2))
 		{
-			s3[i] = s2[i];
-			i++;
+			s3 = malloc(n + 1);
+			for (i = 0; i < n; i++)
+				s3[i] = s2[i];
+			s3[i + 1] = '\0';
+			return (s3);
 		}
-		s3[i + 1] = s4[0];
-		return (s3);
-	}
-
-	while (s1[i])
-	{
-		s3[i] = s1[i];
-		i++;
-	}
-	if (s2 == NULL)
-		return (s3);
-
-	if (n < strlen(s2))
-	{
-		for (k = i; k < (i + n); k++)
+		else
 		{
-			s3[k] = s2[k - i];
+			s3 = malloc(j);
+			for (i = 0; s2[i]; i++)
+				s3[i] = s2[i];
+			return (s3);
 		}
 	}
 	else
 	{
-		for (k = i; k < (i + j); k++)
+		if (n >= j)
 		{
-			s3[k] = s2[k - i];
+			s3 = malloc(sizeof(s1) + j);
+			for (i = 0; s1[i]; i++)
+				s3[i] = s1[i];
+			for (k = i; s2[k - i]; k++)
+				s3[k] = s2[k - i];
+			return (s3);
+		}
+		else
+		{
+			s3 = malloc(size);
+			for (i = 0; s1[i]; i++)
+				s3[i] = s1[i];
+			for (k = i; (k - i) < n;  k++)
+				s3[k] = s2[k - i];
+			s3[k + 1] = '\0';
+			return (s3);
 		}
 	}
-	return (s3);
-
 }
