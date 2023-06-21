67aa6c1728b3f7bd4dfd2fc5ee023d37ede55440
SenpaiItadori <takudzwarnyoni@gmail.com>
2023-06-21 03:21:56 -0700

    To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < b; i++)
 	{
-		opcode = *(unsigned char)address;
+		opcode = *(unsigned char *)address;
 		printf("%.2x", opcode);
 
 		if (i == b - 1)
