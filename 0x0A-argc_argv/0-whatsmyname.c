f8a3d211f1abbd0ef8419340079990d2d4744921
root <takudzwarnyoni00@gmail.com>
2023-08-04 03:48:02 -0700

    it is what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints its name followed by a new line
+ * @argc: the nemuber of command line arguments
+ * @argv: an array containing all command line arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Command line arguments , these are fun and that is exactly what this folder is all about
