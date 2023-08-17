c241e3e8756a2d61c872f217f71cfb160f845c78
root <takudzwarnyoni00@gmail.com>
2023-08-17 12:45:24 -0700

    To be the value of a variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,9 +12,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
-	if (n <= 0)
-		return;
-
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,6 +5,16 @@
  * @format: is a list of types of args passed to the func
  */
 
-void print_all(c)
+void print_all(const char * const format, ...)
 {
+	va_list args;
+	unsigned int i = 0;
+	char *str, *separator = "";
+
+	if (format)
+	{
+		while ()
+		{
+		}
+	}
 }
