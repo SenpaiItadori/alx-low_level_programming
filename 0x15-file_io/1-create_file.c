6924bf83a0201d0dbb2c2d1b4c140d4db8f71695
root <takudzwarnyoni00@gmail.com>
2023-09-04 06:29:19 -0700

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDWR | O_CREAT);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 			i++;
 
 		bytes = write(fd, text_content, i);
-		if (bytes == 0)
+		if (bytes == -1)
 			return (-1);
 	}
 	close(fd);
