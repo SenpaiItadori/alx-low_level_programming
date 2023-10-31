28bc2f67fd5704b7aa4222aee64ef24ba618fbb0
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-10-31 09:54:40 -0700

    the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,35 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, product, first, second;
 
 	for (i = 0; i < 10; i++)
 	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j != 0)
+			{
+				product = i * j;
+				first =  product / 10;
+				second = product % 10;
+				if (first != 0)
+					_putchar(first + '0');
+				else
+					_putchar(' ');
+				_putchar(second + '0');
+				if (j != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar('0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
 }
