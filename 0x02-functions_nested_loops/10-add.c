adca879e1b44a1ea1af2b3379c063c0833c5a942
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-10-31 06:32:44 -0700

    yoooooo

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers and returns the result
+ * @a: one number
+ * @b: the other num
+ * Return: the sum
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+	}
+}
