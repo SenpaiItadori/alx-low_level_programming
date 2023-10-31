b65761c4a24e0491380088fd6640dba56c912453
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-10-31 05:33:34 -0700

    last digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * abs - computes the absolute value fo an integer
+ * @n: the integer
+ *
+ * Return: the absolute val of a intt
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number
+ *
+ * Return: the last digit of n
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int i;
+
+	if (n < 0)
+		n = n * -1;
+	i = (n % 10) + 48;
+	_putchar(i);
+	return (n % 10);
+}
