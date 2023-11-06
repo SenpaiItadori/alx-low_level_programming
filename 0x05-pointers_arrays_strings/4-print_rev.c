3f3a0de1311081f8cc0d4030b64f0fe2f06aa73a
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-06 00:48:59 -0800

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string to be printed using _putchar
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+	_putchar('\n');
+}
