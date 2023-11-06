c4bc50f9f00a33bf7c42d9695f605b7c46cdc221
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-06 00:19:20 -0800

    0.98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of an int via a pointer
- * @n: the int to be updated
- * Return: n/a
+ * reset_to_98 - updates the value of a pointer to an int to 98
+ * @n: the pointe to an int to be changed to 98
+ *
  */
+
 void reset_to_98(int *n)
 {
-	int *p = &*n;
-
-		*p = 98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,4 @@
-arrays pointers and strings this time
+This folder is about pointers arrays and strings.
+Roses are red , violets are blue
+Pointers are easy, AAAAAAAAAAAAAAAAAAA...
+Segmantation Fault.
