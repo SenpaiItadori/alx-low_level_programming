ac123db837bf8f5490a071325fff835ff0f3c9eb
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-08 01:38:23 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a n bytes of a string
+ * @dest: the string to store the copy
+ * @src: the string to be copied
+ * @n: the number of bytes to be copied
+ *
+ * Return: returns the copy of the string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, len = 0;
+
+	while (src[len])
+	{
+		len++;
+	}
+
+	len++;
+	if (len > n)
+		len = n + 1;
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
