5d30d4844766334155b738d4bb6f9d0b39e7ff5d
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-08 01:57:13 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: one string
+ * @s2: another string
+ *
+ * Return: 0 if the strings are equal
+ * num if the first non matching char of s1 is greater than s2s
+ * -num if the first non matching char of s1 is lower than s2s
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0, k;
+
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
+
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+
+	for (k = 0; k <= i; k++)
+	{
+		return (s1[k] - s2[k]);
+	}
+	return (0);
+}
