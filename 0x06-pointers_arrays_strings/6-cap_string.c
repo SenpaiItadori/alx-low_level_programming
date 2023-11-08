6400ca9231e9c24bc7a47675374c0a00dd514ca4
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-08 02:46:58 -0800

    Capitalise on what comes next

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all the words of a string
+ * @str: the string with the words to be caped
+ *
+ * Return: a pointer to a string wth caped letters
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, j = 0;
+	char except[] = {' ', '\t', '\n', ',', ';', '.', '!',
+		'?', '"', '(', ')', '{', '}'};
+
+	while (str[i])
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			for (j = 0; j < 13; j++)
+			{
+				if (str[i - 1] == except[j])
+				{
+					str[i] = str[i] - 32;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (str);
+}
