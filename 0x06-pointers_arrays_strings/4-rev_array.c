7b6c99cf686b7934cbec4943bc6be6a9f791c85c
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-08 03:08:02 -0800

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,4 +7,15 @@
  */
 
 void reverse_array(int *a, int n)
+{
+	int start = 0, end = n - 1, temp;
+
+	while (start < end)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = temp;
+		start++;
+		end--;
+	}
 }
