c74602e4456f30f3c8a6ace33b4cfb52bca251bb
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-10 00:12:23 -0800

    Strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -21,10 +21,13 @@ char *_strncpy(char *dest, char *src, int n)
 	len++;
 	if (len > n)
 		len = n + 1;
+
 	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
+
+	dest[len] = '\0';
 
 	return (dest);
 }
