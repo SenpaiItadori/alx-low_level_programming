a159f49d6e957e3633b92cf3c8e5db15906ab13d
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-10 00:22:41 -0800

    Strcmp

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *str)
 		{
 			for (j = 0; j < 13; j++)
 			{
-				if (str[i - 1] == except[j])
+				if (str[i - 1] == except[j] || i == 0)
 				{
 					str[i] = str[i] - 32;
 				}
