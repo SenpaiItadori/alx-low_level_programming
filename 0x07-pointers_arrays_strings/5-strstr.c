ce2ec19cffdda7418cce62c2f96b7472a32a7c38
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-13 03:30:40 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the main string
+ * @needle: the substring
+ *
+ * Return: a pointer to the start of the located str or NULL if failed
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int  i, len = 0, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		if (j == _strlen(needle))
+		{
+			i--;
+			return (haystack + i);
+		}
+		for (j = 0; needle[j] >= '\0'; j++)
+		{
+			if (j == 0)
+				len = i;
+			if (haystack[len] != needle[j])
+				break;
+			len = len + 1;
+		}
+	}
+	return (NULL);
+}
