d200feec7b9e05bf3b14779d717261c238ed335e
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-13 03:34:28 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,12 +14,12 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i]; i++)
 	{
-		if (j == _strlen(needle))
+		if (j == strlen(needle))
 		{
 			i--;
 			return (haystack + i);
 		}
-		for (j = 0; needle[j] >= '\0'; j++)
+		for (j = 0; needle[j]; j++)
 		{
 			if (j == 0)
 				len = i;
