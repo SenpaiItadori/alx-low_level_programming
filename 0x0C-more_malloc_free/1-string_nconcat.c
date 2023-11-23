6d54d00a293b4e36b0433419b3b33d6df765f4cb
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-23 02:18:39 -0800

    String_concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -38,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		j = len1 + n + 1;
+		j = len1 + n;
 		s3 = malloc(sizeof(char) * j);
 		if (s3 == NULL)
 			return (NULL);
