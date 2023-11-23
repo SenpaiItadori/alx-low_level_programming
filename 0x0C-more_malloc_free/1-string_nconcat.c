dd8720a5b6e04090e0662e3811956aa022440ecd
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-23 02:27:04 -0800

    String_concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,11 +32,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			if (i <= len1)
 				s3[i] = s1[i];
-			if (i >= len1)
-			{
-				s3[i] = s2[k];
-				k++;
-			}
+			else
+				s3[i] = s2[i - len1];
 		}
 	}
 	else
@@ -51,7 +48,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 				s3[i] = s1[i];
 			if (i >= len1)
 			{
-				s3[i] = s2[i - len1];
+				s3[i] = s2[k];
 				k++;
 			}
 		}
