76492dd4e3b53378e8741a4ca8d7137c1fbd09e1
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-23 02:16:17 -0800

    String_concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: the main string
+ * @s2: the concatenator
+ * @n: the first number of bytes to be concated from s2
+ *
+ * Return: a pointer to the new mem with s1 and bytes of s2
+ * or NULL if failed
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s3;
+	unsigned int len1, len2, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (n >= len2)
+	{
+		j = len1 + len2;
+		s3 = malloc(sizeof(char) * j);
+		if (s3 == NULL)
+			return (NULL);
+		for (i = 0; i < j; i++)
+		{
+			if (i < len1)
+				s3[i] = s1[i];
+			else
+				s3[i] = s2[i - len1];
+		}
+	}
+	else
+	{
+		j = len1 + n + 1;
+		s3 = malloc(sizeof(char) * j);
+		if (s3 == NULL)
+			return (NULL);
+		for (i = 0; i < j; i++)
+		{
+			if (i < len1)
+				s3[i] = s1[i];
+			else
+				s3[i] = s2[i - len1];
+		}
+	}
+	s3[j] = '\0';
+	return (s3);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string
+ *
+ * Return: the length of str
+ *
+ */
+
+unsigned int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+
+	return (i);
+}
