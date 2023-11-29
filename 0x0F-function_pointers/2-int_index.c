94927fcbac87f9eaf7e54d79b93fef32172c63c5
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-11-29 13:05:44 -0800

    To hell with circumstances; I create oppotunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,21 +5,21 @@
  * @array: array of ints
  * @size: the size of the array
  * @cmp: a pointer to a function that compares values
- * 
+ *
  * Return: -1 if no matches are found else, index number of the int
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, returns;
+	int i, res;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		returns = cmp(array[i]);
-		if (returns != 0)
+		res = cmp(array[i]);
+		if (res != 0)
 			return (i);
 	}
 
