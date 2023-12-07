955a3e7410cafa05f9e71b7a23fef304f03b80d6
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2023-12-06 23:55:50 -0800

    add_node_end

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: the list to be freed
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *curr;
+
+	while (head)
+	{
+		curr = head->next;
+		free(head->str);
+		free(head);
+		head = curr;
+	}
+}
