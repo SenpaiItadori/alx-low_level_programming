0f1e997729d11bec7570c25273a1e977fdaafb18
SenpaiItadori <takudzwarnyoni00@gmail.com.com>
2024-01-25 03:17:00 -0800

    debug 2

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,10 +10,10 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
+
 	dlistint_t *curr = *head;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	for (; index != 0; index--)
