5bb47c72cff065b9ea8c7e1f7daa64bf2c7538be
SenpaiItadori <takudzwarnyoni00@gmail.com>
2024-02-16 02:31:14 -0800

    hash get value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,11 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table to be looked into
+ * @key: the key i will be looking for
+ *
+ * Return: the value to the corresponding key or NULL if not found
  *
  */
 
@@ -12,5 +17,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	position = key_index((const unsigned char *)key, ht->size)
+	position = key_index((const unsigned char *)key, ht->size);
+	if (position >= ht->size)
+		return (NULL);
+
+	curr = ht->array[position];
+	while (curr && strcmp(curr->key, key) != 0)
+		curr = curr->next;
+
+	if (curr == NULL)
+		return (NULL);
+	return (curr->value);
 }
